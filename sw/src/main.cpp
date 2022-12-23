4b0625a82fcf9addaf9a4c3c37f2c5344cced6b2
bartek 'basz' szurgot <git@baszerr.eu>
2022-12-23 17:38:25 +0100

    workign draft where color and brightness can be set

--- sw/src/main.cpp
+++ sw/src/main.cpp
@@ -2,9 +2,9 @@
 #include <Adafruit_NeoPixel.h>
 
 auto const g_led_count = 4;
-auto const g_led_pin = D7;
+auto const g_led_pin = D4;
 
-Adafruit_NeoPixel g_pixels{g_led_count, g_led_pin, NEO_GRB + NEO_KHZ800};  // TODO: | ??
+Adafruit_NeoPixel g_pixels{g_led_count, g_led_pin, NEO_GRB | NEO_KHZ800};
 
 struct Color
 {
@@ -17,20 +17,30 @@ struct Color
 auto randomize_color()
 {
   Color c;
+  c = Color{255, 0, 255};
   // TODO: random fire stuff
   return c;
 }
 
 
+auto randomize_brightness()
+{
+  uint8_t b{10};
+  return b;
+}
+
+
 void update_leds()
 {
-  for(auto i=0; i<g_led_count; ++i)
-  {
-    auto const c = randomize_color();
-    g_pixels.setPixelColor(i, c.r, c.g, c.b);
-  }
-
   g_pixels.clear();
+
+  auto const c = randomize_color();
+  for(auto i=0; i<g_led_count; ++i)
+    g_pixels.setPixelColor(i, c.r, c.g, c.b);
+
+  auto const b = randomize_brightness();
+  g_pixels.setBrightness(b);
+
   g_pixels.show();
 }
 
