597e51d97b8976b927c3edb9df3d8e6035f39ab8
bartek 'basz' szurgot <git@baszerr.eu>
2022-12-23 19:29:14 +0100

    added olor commorhing + decreased delay in main()

--- sw/src/ColorRandomizer.hpp
+++ sw/src/ColorRandomizer.hpp
@@ -7,12 +7,42 @@ struct ColorRandomizer
 {
   auto randomize()
   {
-    auto constexpr n = sizeof(palette_) / sizeof(palette_[0]);
-    state_ = (state_ + 1) % n;
-    return palette_[state_];
+    current_ = smooth_to_next();
+    smoothed_ += 1;
+
+    if(smoothing_ == smoothed_)
+    {
+      auto constexpr n = sizeof(palette_) / sizeof(palette_[0]);
+      state_ = (state_ + 3) % n;
+      target_ = palette_[state_];
+      smoothed_ = 0;
+    }
+
+    return current_;
   }
 
 private:
+  uint8_t smooth_to_next(uint8_t a, uint8_t b) const
+  {
+    auto const x = ( a * (smoothing_ - smoothed_) ) / smoothing_;
+    auto const y = ( b * smoothed_ ) / smoothing_;
+    auto const out = x + y;
+    if(out < 0)
+      return 0;
+    if(out > 0xff)
+      return 0xff;
+    return static_cast<uint8_t>(out);
+  }
+
+  Color smooth_to_next() const
+  {
+    return Color{
+      smooth_to_next(current_.r, target_.r),
+      smooth_to_next(current_.g, target_.g),
+      smooth_to_next(current_.b, target_.b),
+    };
+  }
+
   Color const palette_[7] = {
     {0xb7, 0x5b, 0x01},
     {0xce, 0x73, 0x02},
@@ -25,4 +55,9 @@ private:
   };
 
   uint16_t state_{0};
+
+  Color current_{palette_[0]};
+  Color target_{palette_[1]};
+  int16_t const smoothing_{3};
+  int16_t smoothed_{0};
 };
